d8d4fd6d46ceceda5e535d42619dcfd72f0ff0e9
neuron-git-desktop <praveenneuron@gmail.com>
2016-03-14 18:02:27 +0100

    Adding doxyfile config

--- documentation/generate_documents.bat
+++ documentation/generate_documents.bat
@@ -2,3 +2,8 @@ rmdir /S /Q html
 echo generating documentation
 D:\InstalledPrograms\doxygen\bin\doxygen Doxyfile
 echo documentation completed :)
+echo committing to repo
+cd E:\GitHub\praveenneuron.github.io\writer_identification_doc
+git add -A
+git commit "auto commit"
+git push
