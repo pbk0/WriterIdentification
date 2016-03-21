45c35e7d44e3bae97cac9c74616761ea1e28b19e
neuron-git-desktop <praveenneuron@gmail.com>
2016-03-21 14:44:42 +0100

    trained rbm

--- source/rbm_learn_model.py
+++ source/rbm_learn_model.py
@@ -1,9 +1,15 @@
 """
+Code to train generative RBM model
 
+You just need to run this file to train the model
+Learned model is saved as weights in data/rbm_learned_weights/ directory
+We dump learned weights after every 100 epochs
+
+Later this learned weights can be used to generate hidden activities
 """
 
 from __future__ import division
-from data_handling import load_features_and_labels
+from source.utility.data_handling import load_features_and_labels
 import time
 import numpy as np
 #import util
@@ -12,7 +18,8 @@ import numpy as np
 g_train, g_train_label, g_test, g_test_label, g_feature_name = load_features_and_labels()
 #util.load('mnist.dat', globals())
 #dat = dat/255.
-dat = np.transpose(np.asarray(g_train))
+#dat = np.transpose(np.asarray(g_train))
+dat = np.transpose(np.asarray(np.concatenate((g_train, g_test))))
 
 # training parameters
 epsilon = 0.01
@@ -80,10 +87,10 @@ for epoch in range(num_epochs):
     print("Mean squared error: %f" % np.mean(err))
     print("Time: %f" % (time.time() - start_time))
 
-    if epoch % 100 == 0:
-        np.save('rbm_numpy/_' + str(epoch) + '_numpy_w_vh_', w_vh)
-        np.save('rbm_numpy/_' + str(epoch) + '_numpy_w_v_', w_v)
-        np.save('rbm_numpy/_' + str(epoch) + '_numpy_w_h_', w_h)
-        np.save('rbm_numpy/_' + str(epoch) + '_numpy_err_', np.mean(err))
+    if epoch % 10 == 0:
+        np.save('../data/rbm_learned_weights/_' + str(epoch) + '_numpy_w_vh_', w_vh)
+        np.save('../data/rbm_learned_weights/_' + str(epoch) + '_numpy_w_v_', w_v)
+        np.save('../data/rbm_learned_weights/_' + str(epoch) + '_numpy_w_h_', w_h)
+        np.save('../data/rbm_learned_weights/_' + str(epoch) + '_numpy_err_', np.mean(err))
 
 
